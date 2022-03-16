480a6007b98810dfd8dea5e996c753ff876e0f32
aren0416 <aren0416@gmail.com>
2022-03-16 20:42:42 +0900

    kakao practice2

--- src/components/Kakao.js
+++ src/components/Kakao.js
@@ -5,6 +5,7 @@ export const Kakao = ({ kakaos }) => {
     <ul className="kakao">
       {kakaos.map((kakaos) => (
         <li key={kakaos.id}>
+          <div>{kakaos.img}</div>
           <h1>{kakaos.title}</h1>
           <p>{kakaos.description}</p>
         </li>
